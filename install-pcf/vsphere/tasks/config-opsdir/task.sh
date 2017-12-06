c460a0069fd6998fd3b936eeeb0df162378cc17d
Simon O'Brien <simon@the-obriens.net>
2017-12-06 13:49:03 +0000

    updated to support single network

--- install-pcf/vsphere/tasks/config-opsdir/task.sh
+++ install-pcf/vsphere/tasks/config-opsdir/task.sh
@@ -91,20 +91,6 @@ network_configuration=$(
       "icmp_checks_enabled": $icmp_checks_enabled,
       "networks": [
         {
-          "name": $infra_network_name,
-          "service_network": false,
-          "subnets": [
-            {
-              "iaas_identifier": $infra_vcenter_network,
-              "cidr": $infra_network_cidr,
-              "reserved_ip_ranges": $infra_reserved_ip_ranges,
-              "dns": $infra_dns,
-              "gateway": $infra_gateway,
-              "availability_zones": ($infra_availability_zones | split(","))
-            }
-          ]
-        },
-        {
           "name": $deployment_network_name,
           "service_network": false,
           "subnets": [
@@ -115,34 +101,6 @@ network_configuration=$(
               "dns": $deployment_dns,
               "gateway": $deployment_gateway,
               "availability_zones": ($deployment_availability_zones | split(","))
-            }
-          ]
-        },
-        {
-          "name": $services_network_name,
-          "service_network": $services_network_is_service_network,
-          "subnets": [
-            {
-              "iaas_identifier": $services_vcenter_network,
-              "cidr": $services_network_cidr,
-              "reserved_ip_ranges": $services_reserved_ip_ranges,
-              "dns": $services_dns,
-              "gateway": $services_gateway,
-              "availability_zones": ($services_availability_zones | split(","))
-            }
-          ]
-        },
-        {
-          "name": $dynamic_services_network_name,
-          "service_network": true,
-          "subnets": [
-            {
-              "iaas_identifier": $dynamic_services_vcenter_network,
-              "cidr": $dynamic_services_network_cidr,
-              "reserved_ip_ranges": $dynamic_services_reserved_ip_ranges,
-              "dns": $dynamic_services_dns,
-              "gateway": $dynamic_services_gateway,
-              "availability_zones": ($dynamic_services_availability_zones | split(","))
             }
           ]
         }
